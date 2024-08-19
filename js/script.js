11b238a0d93a4a2789e38a105d6ee76dfeb4cdc7
Mohammad Arman Ali <arman.ali.13000@gmail.com>
2024-08-19 19:42:04 +0530

    some changes

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 var typed = new Typed(".typing", {
-    strings:["","Software Engineer","Web Designer","Web Developer",],
+    strings:["","Software Engineer","Java Developer","Full Stack Java Develoepr"],
     typeSpeed:100,
     BackSpeed:60,
     loop:true
